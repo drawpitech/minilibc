dc1c26661565cfc40f4bd46e9b8159b7086bdd52
Clément <clement2104.boillot@gmail.com>
2024-02-08 15:01:27 +0100

    Add all the types for function ptr of the subject

--- tests/tests.h
+++ tests/tests.h
@@ -12,24 +12,31 @@
 #include <stddef.h>
 
 #define TestLibShared(func_name, test_name)                                 \
-    void nicest_test_ever_##func_name##test_name(my_##func_name rawFunc);   \
+    void nicest_test_ever_##func_name##_##test_name(func_name##_t rawFunc); \
                                                                             \
     Test(func_name, test_name)                                              \
     {                                                                       \
         void *handle = dlopen("./libasm.so", RTLD_LAZY);                    \
         if (!handle)                                                        \
             cr_skip("Failed to open shared library: %s", dlerror());        \
-        my_##func_name rawFunc = (my_##func_name)dlsym(handle, #func_name); \
+        func_name##_t rawFunc = dlsym(handle, #func_name);                  \
         if (!rawFunc)                                                       \
             cr_skip("Failed to get function pointer: %s", dlerror());       \
-        nicest_test_ever_##func_name##test_name(rawFunc);                   \
+        nicest_test_ever_##func_name##_##test_name(rawFunc);                \
         dlclose(handle);                                                    \
     }                                                                       \
                                                                             \
-    void nicest_test_ever_##func_name##test_name(my_##func_name rawFunc)
+    void nicest_test_ever_##func_name##_##test_name(func_name##_t rawFunc)
 
-typedef size_t (*my_strlen)(char *str);
-typedef char *(*my_strchr)(const char *s, int c);
-typedef char *(*my_strrchr)(const char *s, int c);
-typedef void *(*my_memset)(void *s, int c, size_t n);
-typedef void *(*my_memcpy)( void *restrict dest, const void *restrict src, size_t n);
+typedef size_t (*strlen_t)(char *str);
+typedef char *(*strchr_t)(const char *s, int c);
+typedef char *(*strrchr_t)(const char *s, int c);
+typedef void *(*memset_t)(void *s, int c, size_t n);
+typedef void *(*memcpy_t)(void *dest, const void *src, size_t n);
+typedef int (*strcmp_t)(const char *s1, const char *s2);
+typedef void *(*memmove_t)(void *dest, const void *src, size_t n);
+typedef int (*strncmp_t)(const char *s1, const char *s2, size_t n);
+typedef int (*strcasecmp_t)(const char *s1, const char *s2);
+typedef char *(*strstr_t)(const char *haystack, const char *needle);
+typedef char *(*strpbrk_t)(const char *s, const char *accept);
+typedef size_t (*strcspn_t)(const char *s, const char *reject);
