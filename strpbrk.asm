ffc55f51da1c0c4f0415347f884e024688c1bd37
Clément <clement2104.boillot@gmail.com>
2024-02-25 22:51:33 +0100

    Fix strpbrk
    i didnt find anything that really worked... fuck

--- strpbrk.asm
+++ strpbrk.asm
@@ -6,11 +6,44 @@
 ;;
 
 SECTION .text
-EXTERN strcspn
+EXTERN strchr
+
+strcspn:
+  PUSH rdx
+  PUSH rcx
+  PUSH rsi
+
+  MOV rdx, rdi ; save rdi
+  MOV rdi, rsi ; move rsi to rdi to use strchr
+
+  XOR rcx, rcx ; i
+
+.loop:
+  CMP BYTE [rdx + rcx], 0
+  JE .end
+
+  ; move arr[i] to rsi
+  MOV sil, BYTE [rdx + rcx]
+  CALL strchr wrt ..plt
+  CMP rax, 0
+  JNE .end
+
+  INC rcx
+  JMP .loop
+
+.end:
+  POP rsi
+  MOV rax, rcx
+  POP rcx
+
+  MOV rdi, rdx
+  POP rdx
+
+  RET
 
 GLOBAL strpbrk
 strpbrk:
-  CALL strcspn wrt ..plt
+  CALL strcspn
   ADD rax, rdi
   CMP BYTE [rax], 0
   JNE .end
